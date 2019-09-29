156b0d89bf1fd7f03e4bd50da4db0153fd1a2976
Michael "Hysteria" Gorshenin <miklehigaran@gmail.com>
2019-09-29 22:44:23 +0300

    Spring Initializr commit with WEB, MySQL, Cassandra and Actuator dependencies

---
+++ src/main/java/com/is42/fmms/FmmsApplication.java
@@ -0,0 +1,13 @@
+package com.is42.fmms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmmsApplication.class, args);
+	}
+
+}
